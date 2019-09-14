e8e6f59f60d5d822a66e4a3c6076e94c0fee03d2
Sitthikorn Maneesut <sitthikornjuni@gmail.com>
2019-09-14 17:46:53 +0700

    Update logintest

--- _func.js
+++ _func.js
@@ -3,12 +3,18 @@ const md5 = require('md5');
 module.exports.login = (req, res) => {
     let data = req.body;
     console.log(`- Some one login in.`);
-    console.log(data);
+    console.log(`-- It's name :: ${data.username}.`);
     let response = {
         firstname: 'Sitthikorn',
         lastname: 'Maneesut',
         title: 'Mr.',
         uid: md5(1212)
     };
-    res.send({ code: 200, message: `Login succcess`, data: response })
+    if (data.username == "admin" && data.password == md5('1234')) {
+        res.status(200).send({ code: 200, message: `Login succcess`, data: response })
+    }
+    else{
+        res.status(401).send({ code: 401, message: `Login unsucccess` });
+        // res.send({ code: 401, message: `Login unsucccess` })
+    }
 }
